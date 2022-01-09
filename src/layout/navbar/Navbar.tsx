fbffa6c920409627467be1482ce0ae995dea6c2b
Soham Sarkar <soham.ss1@gmail.com>
2022-01-09 10:12:14 +0530

    navbar

--- src/layout/navbar/Navbar.tsx
+++ src/layout/navbar/Navbar.tsx
@@ -1,5 +1,21 @@
+import {Flex, Heading, Text} from '@chakra-ui/react'
 import React from 'react'
 
+const navigation = [
+  {label: 'Work.'},
+  {label: 'About Me.'},
+  {label: 'Contact Me.'},
+]
+
 export default function Navbar() {
-  return <div></div>
+  return (
+    <Flex alignItems="center" justifyContent={'space-between'} px={28} py={12}>
+      <Heading>UI.joe</Heading>
+      <Flex>
+        {navigation.map(ele => (
+          <Text key={ele.label}>{ele.label}</Text>
+        ))}
+      </Flex>
+    </Flex>
+  )
 }
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,18 @@
-import '../styles/globals.css'
 import type {AppProps} from 'next/app'
 import {ChakraProvider} from '@chakra-ui/react'
 import {theme} from '../styles/chakra-theme'
+import '@fontsource/manrope/500.css'
+import '@fontsource/manrope/700.css'
+import '../styles/globals.css'
+import Navbar from 'layout/navbar/Navbar'
+import Footer from 'layout/footer/Footer'
 
 function MyApp({Component, pageProps}: AppProps) {
   return (
     <ChakraProvider theme={theme}>
+      <Navbar />
       <Component {...pageProps} />
+      <Footer />
     </ChakraProvider>
   )
 }
