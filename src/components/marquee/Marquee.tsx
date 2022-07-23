4dc68dd67d9109f3ef9f34834780b8ec03d3f7ac
Soham Sarkar <soham.ss1@gmail.com>
2022-07-23 18:26:46 +0530

    feat: random grad gen

--- src/components/marquee/Marquee.tsx
+++ src/components/marquee/Marquee.tsx
@@ -1,36 +1,39 @@
 import * as React from 'react'
 import FastMarquee from 'react-fast-marquee'
 import {Box, Heading} from '@chakra-ui/react'
+import {generateGrad} from 'utils'
 
 export default function Marquee() {
-  // const [play, setPlay] = React.useState(true)
-
   return (
     <Box mx={['-20px', '-125px']} mt={['30px', '140px']} mb={['-8px', '0px']}>
-      <FastMarquee
-        pauseOnHover={true}
-        pauseOnClick={true}
-        // gradient={true}
-        // gradientColor={[230, 180, 120]}
-      >
+      <FastMarquee pauseOnHover={true} pauseOnClick={true}>
         <Heading
-          textShadow={'-1px 0 black, 0 1px black, 1px 0 black, 0 -1px black'}
           my={[0, 8]}
           color="white"
           fontSize={['90px', '150px']}
           py={[0, 4]}
           zIndex={10}
-          _hover={{
-            bgGradient:
-              'linear-gradient(141.68deg, #FF5EF9 -14.47%, #FF5EF9 15.43%, #C77FFC 45.9%, #5FBFFC 71.77%, #0EEEFF 95.91%)',
-          }}
-          // bgClip="text"
-          // onMouseEnter={() => setPlay(false)}
-          // onMouseLeave={() => setPlay(true)}
+          textShadow={'-1px 0 black, 0 1px black, 1px 0 black, 0 -1px black'}
         >
-          Designs.Emotions.Happiness.
+          {textArr.map(char => (
+            <Box
+              as="span"
+              key={char}
+              _hover={{
+                bgGradient: generateGrad(),
+                bgClip: 'text',
+                textShadow: 'none',
+              }}
+            >
+              {char}
+            </Box>
+          ))}
         </Heading>
       </FastMarquee>
     </Box>
   )
 }
+
+const text = 'Designs.Emotions.Happiness.'
+
+const textArr = [...text]
